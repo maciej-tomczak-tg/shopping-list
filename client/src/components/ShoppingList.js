b05c43d4aa84ac962d6a95d72a6eacf1ee56bbfc
mtomczak <maciej.tomczak@edgeteq.com>
2020-09-03 13:39:02 +0200

    add removing

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -2,12 +2,18 @@ import React, { useState } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import ListGroupItem from "react-bootstrap/ListGroupItem";
 import AddElement from "./AddElement";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faTrash} from "@fortawesome/free-solid-svg-icons";
 
 function ShoppingList() {
     const [items, setItems] = useState([]);
 
-    function addItem(element) {
-        setItems([...items, element]);
+    function addItem(item) {
+        setItems([...items, item]);
+    }
+
+    function removeItem(item) {
+        setItems(items.filter(itm => item.id !== itm.id));
     }
 
     return (
@@ -15,7 +21,9 @@ function ShoppingList() {
             <AddElement addItem={addItem}/>
             <ListGroup>
                 {items.map(itm => {
-                    return <ListGroupItem key={itm.id}>{itm.name}</ListGroupItem>
+                    return <ListGroupItem key={itm.id}>{itm.name}
+                        <FontAwesomeIcon className="float-right" icon={faTrash} onClick={() => removeItem(itm)} />
+                    </ListGroupItem>
                 })}
             </ListGroup>
         </div>
